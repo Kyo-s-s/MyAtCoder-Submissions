3aedd3a0d0a5f7be5b2b9443651956cfcf384a5b
AutoCloneBot <acautoclone@bot.com>
2022-10-09 14:53:28 +0000

    Added my codes from AtCoder

--- arc150/arc150_a.cpp
+++ arc150/arc150_a.cpp
@@ -97,59 +97,97 @@ template<class Head, class... Tail> void FLASH(const Head  &head, const Tail &..
 
 #pragma endregion header    
 
+template<class M> struct Segtree {
+  public:
+    using S = typename M::T;
+
+    Segtree() : Segtree(0) {}
+    Segtree(vector<S> v) : n(int(v.size())) { 
+        while((1 << log) < n) log++;
+        size = 1 << log;
+        d = vector<S> (2 * size, M::e());
+        for(int i = 0; i < n; i++) d[size + i] = v[i];
+        for(int i = size- 1; i >= 1; i--) update(i);
+    }
+    Segtree(int n) : Segtree(vector<S> (n, M::e())) {}
+
+    void set(int p, S x){
+        assert(0 <= p && p < n);
+        p += size;
+        d[p] = x;
+        for(int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p){
+        assert(0 <= p && p < n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r){
+        assert(0 <= l && l <= r && r <= n);
+        S sml = M::e(), smr = M::e();
+        l += size; r += size;
+        while(l < r){
+            if(l & 1) sml = M::op(sml, d[l++]);
+            if(r & 1) smr = M::op(d[--r], smr);
+            l >>= 1; r >>= 1;
+        }
+        return M::op(sml, smr);
+    }
+
+    S all_prod(){ return d[1]; }
+
+
+  private:
+    int n, size, log = 0;
+    vector<S> d;
+    void update(int k){ d[k] = M::op(d[k * 2], d[k * 2 + 1]); }
+
+};
+
+struct Monoid {
+    using T = ll;
+    static T op(T a, T b){ return a + b; }
+    static T e(){ return 0; }
+};
+
+
+void solve() {
+    INT(N, K);
+    STR(S);
+
+    Segtree<Monoid> segZ(N), segO(N);
+
+    rep(i, N) {
+        if (S[i] == '0') segZ.set(i, 1);
+        if (S[i] == '1') segO.set(i, 1);
+    }
+
+    ll i = -1;
+    ll cnt = 0;
+    while (true) {
+        i++;
+        ll j = i + K;
+        if (j > N) break;
+
+        // その範囲内に0がいないか？
+        if (segZ.prod(i, j) != 0) continue;
+
+        // その範囲外に1がいないか？
+        if (segO.all_prod() != segO.prod(i, j)) continue;
+
+        cnt++;
+
+    }
+
+    YesNo(cnt == 1);
+
+}
+
+
 int main() {
 
-    LL(N, M);
-
-    vvll A(N, vll(N, INF));
-
-    deque<pii> que;
-    que.pb({0, 0});
-    A[0][0] = 0;
-
-    vpii go;
-    
-    map<ll, ll> S;
-    ll x = 0;
-    while (x * x <= M) {
-        S[x * x] = x;
-        x++;
-    }
-    for (auto [a, x]: S) if (x < N) {
-        if (S.find(M - a) != S.end()) {
-            ll b = S[M - a];
-            if (b < N) {
-                go.pb({x, b});
-                go.pb({-x, b});
-                go.pb({x, -b});
-                go.pb({-x, -b});
-            }
-        }
-    }
-
-
-    vvll vis(N, vll(N, false));
-    while (!que.empty()) {
-        auto [x, y] = que.front(); que.pop_front();
-        vis[x][y] = true;
-
-        for (auto [a, b]: go) {
-
-            ll nx = x + a, ny = y + b;
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if (chmin(A[nx][ny], A[x][y] + 1)) {
-                que.pb({nx, ny});
-            }
-        }
-    }
-
-
-    for (auto a: A) {
-        for (auto& b: a) {
-            if (b == INF) b = -1;
-        }
-        OUT(a);
-    }
-
+    LL(T);
+    while (T--) solve();
 
 }
