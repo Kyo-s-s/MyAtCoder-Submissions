005355e528cddbc8a24f8395e943d855f8990955
AutoCloneBot <acautoclone@bot.com>
2022-12-14 14:49:27 +0000

    Added my codes from AtCoder

---
+++ abc194/abc194_c.rs
@@ -0,0 +1,81 @@
+use std::collections::BTreeMap;
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        n: usize,
+        a: [i32; n]
+    }
+    let a: Vec<i32> = a;
+
+    let mut map = BTreeMap::new();
+    for i in 0..n {
+        let count = map.entry(a[i]).or_insert(0);
+        *count += 1;
+    }
+
+    let mut ans = 0;
+    for x in -200..=200 {
+        for y in -200..=200 {
+            let cx = map.entry(x).or_insert(0).clone();
+            let cy = map.entry(y).or_insert(0).clone();
+            let x = x as i64;
+            let y = y as i64;
+            ans += (x - y) * (x - y) * (cx) * (cy);
+        }
+    }
+    ans /= 2;
+    println!("{}", ans);
+}
