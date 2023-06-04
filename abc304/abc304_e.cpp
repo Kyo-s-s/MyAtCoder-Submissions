719111aea38833d82a87d069d6c32d3669423c72
AutoCloneBot <acautoclone@bot.com>
2023-06-04 14:49:37 +0000

    Added my codes from AtCoder

---
+++ abc304/abc304_e.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vb = vector<bool>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<ll, ll>>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<long long>>;
+using vvd = vector<vector<double>>;
+using vvs = vector<vector<string>>;
+using vvb = vector<vector<bool>>;
+template<typename T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+
+/* define */
+#define MOD 998244353
+// #define MOD 1000000007
+#define INF (1LL << 60)
+#define elif else if
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) cout << (bool ? "YES\n" : "NO\n")
+#define YesNo(bool) cout << (bool ? "Yes\n" : "No\n")
+#define yesno(bool) cout << (bool ? "yes\n" : "no\n")
+template<class T> bool chmax(T &a, const T &b) {if(a<b){ a=b; return 1;} return 0;}
+template<class T> bool chmin(T &a, const T &b) {if(b<a){ a=b; return 1;} return 0;}
+#define debug(x) cerr << #x << ":" << x << "\n";
+
+/* REP macro */
+#define reps(i, a, n) for(ll i = (a); i < (n); i++)
+#define rep(i, n) reps(i, 0, (n))
+#define rrep(i, n) reps(i, 1, (n + 1))
+#define repd(i, n) for(ll i = (n - 1); i >= 0; i--)
+#define rrepd(i, n) for(ll i = (n); i >= 1; i--)
+#define fore(i, a) for(auto &i: a)
+
+/* vector */
+template<class T> T vmax(vector<T> &array){ T ret = array[0]; for(T a: array) chmax(ret, a); return ret; }
+template<class T> T vmin(vector<T> &array){ T ret = array[0]; for(T a: array) chmin(ret, a); return ret; }
+template<class T> T sum(vector<T> &array){ T ret = 0; for(T a:array) ret += a; return ret; }
+template<class T> void list_set(vector<T> &array){ sort(all(array)); array.erase(unique(all(array)),array.end()); }
+template<class T> int bisect_left(vector<T> &array, T key){ return lower_bound(all(array),key) - array.begin(); }
+template<class T> int bisect_right(vector<T> &array, T key){ return upper_bound(all(array),key) - array.begin(); }
+
+/* string */
+ll string_to_ll(string n){ ll ret = 0, k = 1; while(n.length() > 0){ ret += k * (n.back() - '0'); n.pop_back(); k *= 10; } return ret; }
+string ll_to_string(ll n){ string ret = ""; while(n > 0){ ret.pb((n % 10) + '0'); n /= 10; } reverse(all(ret)); return ret; }
+
+struct popopo{ popopo(){ cin.tie(0); ios::sync_with_stdio(0); cout << fixed << setprecision(15); }; } popopoppo;
+
+// n = 1,...,N に対して、n % A < B を満たすものの数 
+ll Count_of_n_mod_A_less_than_B(ll N, ll A, ll B){ return N / A * min(A, B) + min(N % A, B - 1); }
+
+bool include(ll l, ll r, ll x) { return l <= x && x < r; }
+
+/* IN/OUT */
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &);
+template <class T> void scan(vector<T> &a) { for(auto &i : a) scan(i); }
+
+void IN(){}
+template<class Head, class... Tail> void IN(Head& head, Tail &...tail){ scan(head); IN(tail...); }
+
+#define INT(...) int __VA_ARGS__; IN(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__; IN(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__; IN(__VA_ARGS__)
+#define VEC(type, name, size) vector<type> name(size); IN(name)
+#define VECS(type, name, size) vector<type> name(size + 1); for(int i = 1; i <= size; i++) scan(name[i])
+
+void OUT(){ cout << "\n"; }
+template<class T> void output(T a){ cout << a; }
+template<class T> void output(vector<T> v){ for(int i = 0; i < v.size(); i++) cout << v[i] << (i == v.size() - 1 ? "" : " "); }
+template<class Head, class... Tail> void OUT(const Head &head, const Tail &...tail) { output(head); if(sizeof...(tail)) cout << " "; OUT(tail...); }
+
+void FLASH(){ cout << endl; }
+template<class Head, class... Tail> void FLASH(const Head  &head, const Tail &...tail) { output(head); if(sizeof...(tail)) cout << " "; FLASH(tail...); }
+
+namespace kyo {
+
+long long bisect(long long ok, long long ng, function<bool(long long)> is_ok) { while (abs(ok - ng) > 1) { long long mid = ok + (ng - ok) / 2; (is_ok(mid) ? ok : ng) = mid; } return ok; }
+
+}
+
+struct UnionFind {
+    int n, cnt;
+    vector<int> parent;
+    UnionFind() : n(0), cnt(0) {}
+    UnionFind(int n) : n(n), cnt(n), parent(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < n && 0 <= b && b < n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent[x] < -parent[y]) swap(x, y);
+        parent[x] += parent[y];
+        parent[y] = x;
+        cnt--;
+        return x;
+    } 
+
+    int leader(int a) {
+        assert(0 <= a && a < n);
+        if (parent[a] < 0) return a;
+        return parent[a] = leader(parent[a]);
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < n && 0 <= b && b < n);
+        return leader(a) == leader(b);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < n);
+        return -parent[leader(a)];
+    }
+
+    int count() { return cnt; }
+
+    vector<vector<int>> groups() {
+        vector<int> leader_buf(n), group_size(n);
+        for (int i = 0; i < n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        vector<vector<int>> result(n);
+        for (int i = 0; i < n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            remove_if(result.begin(), result.end(),
+                      [&](const vector<int> &v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+};
+
+
+int main() {
+    
+    LL(N, M);
+    vvll G(N);
+    UnionFind uf(N);
+    rep(i, M) {
+        LL(u, v); u--; v--;
+        G[u].pb(v);
+        G[v].pb(u);
+        uf.merge(u, v);
+    } 
+    LL(K);
+    vpll xy;
+    rep(i, K) {
+        LL(x, y);
+        xy.pb({x - 1, y - 1});
+    } 
+    
+    set<pll> s;
+    for (auto [x, y] : xy) {
+        ll lx = uf.leader(x), ly = uf.leader(y);
+        s.insert({min(lx, ly), max(lx, ly)});
+    }
+
+    // for x, y in xy, G上でxとyが非連結
+
+    LL(Q);
+    while (Q--) {
+        LL(p, q); p--; q--;
+        ll lp = uf.leader(p), lq = uf.leader(q);
+        bool ok = s.find({min(lp, lq), max(lp, lq)}) == s.end();
+        YesNo(ok);
+    }
+
+    
+}
