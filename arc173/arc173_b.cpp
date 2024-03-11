1db0b537093ddbc0a27c367808706e8d758c41ed
AutoCloneBot <acautoclone@bot.com>
2024-03-11 14:49:19 +0000

    Added my codes from AtCoder

---
+++ arc173/arc173_b.cpp
@@ -0,0 +1,267 @@
+#ifdef INCLUDED_MAIN
+
+template<class Cap> struct Dinic {
+  public:
+    Dinic(int n) : n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap) {
+        int m = (int)edges.size();
+        edges.push_back({from, (int)g[from].size()});
+        int from_id = (int)g[from].size();
+        int to_id = (int)g[to].size();
+        if (from == to) to_id++;
+        g[from].push_back(dinic_edge{to, to_id, cap});
+        g[to].push_back(dinic_edge{from, from_id, 0});
+        return m;
+    }
+
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        auto& e = g[edges[i].first][edges[i].second];
+        auto& re = g[e.to][e.rev];
+        e.cap = new_cap - new_flow;
+        re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+
+    Cap flow(int s, int t, Cap flow_limit) {
+        std::vector<int> level(n), iter(n);
+        simple_queue<int> que;
+
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            que.clear();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.pop_front();
+                for (auto e : g[v]) {
+                    if (e.cap > 0 && level[e.to] == -1) {
+                        level[e.to] = level[v] + 1;
+                        if (e.to == t) return;
+                        que.push(e.to);
+                    }
+                }
+            }
+        };
+
+        auto dfs = [&](auto dfs, int v, Cap limit) -> Cap {
+            if (v == s) return limit;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                dinic_edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d = dfs(dfs, e.to, std::min(limit - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == limit) return res;
+            }
+            level[v] = n;
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit - flow);
+            if (!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    Cap get_flow(int i) {
+        auto _e = g[edges[i].first][edges[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return _re.cap;
+    } 
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(n);
+        simple_queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.pop_front();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private: 
+    int n;
+    struct dinic_edge {
+        int to, rev;
+        Cap cap;
+    };
+    template<class T> struct simple_queue {
+        std::vector<T> data;
+        int pos = 0;
+        void clear() {
+            data.clear();
+            pos = 0;
+        }
+        void push(T val) {
+            data.emplace_back(val);
+        }
+        T pop_front() {
+            return data[pos++];
+        }
+        bool empty() {
+            return pos == (int)data.size();
+        }
+    };
+    std::vector<std::pair<int, int>> edges;
+    std::vector<std::vector<dinic_edge>> g;
+};
+
+int main() {
+
+    LL(N);
+    vpll XY(N);
+    rep(i, N) cin >> XY[i].fi >> XY[i].se;
+
+    auto line = [&](int i, int j, int k) -> bool {
+        ll x1 = XY[i].fi, y1 = XY[i].se;
+        ll x2 = XY[j].fi, y2 = XY[j].se;
+        ll x3 = XY[k].fi, y3 = XY[k].se;
+        return (x1 - x2) * (y1 - y3) == (x1 - x3) * (y1 - y2);
+    };
+
+    ll ans = N / 3;
+
+    rep(i, N) rep(j, N) if (i != j) {
+        ll c = 2;
+        rep(k, N) if (i != k && j != k) {
+            if (line(i, j, k)) c++;
+        }
+        if (c >= N / 3 * 2) {
+            ans = min(ans, min(N - c, c / 2));
+        }
+    }
+
+    OUT(ans);
+
+
+}
+
+#else
+
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vb = vector<bool>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<ll, ll>>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<long long>>;
+using vvd = vector<vector<double>>;
+using vvs = vector<vector<string>>;
+using vvb = vector<vector<bool>>;
+template<typename T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+
+/* define */
+#define MOD 998244353
+// #define MOD 1000000007
+#define INF (1LL << 60)
+#define elif else if
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) cout << (bool ? "YES\n" : "NO\n")
+#define YesNo(bool) cout << (bool ? "Yes\n" : "No\n")
+#define yesno(bool) cout << (bool ? "yes\n" : "no\n")
+template<class T> bool chmax(T &a, const T &b) {if(a<b){ a=b; return 1;} return 0;}
+template<class T> bool chmin(T &a, const T &b) {if(b<a){ a=b; return 1;} return 0;}
+#define debug(x) cerr << #x << ":" << x << "\n";
+
+/* REP macro */
+#define reps(i, a, n) for(ll i = (a); i < (n); i++)
+#define rep(i, n) reps(i, 0, (n))
+#define rrep(i, n) reps(i, 1, (n + 1))
+#define repd(i, n) for(ll i = (n - 1); i >= 0; i--)
+#define rrepd(i, n) for(ll i = (n); i >= 1; i--)
+#define fore(i, a) for(auto &i: a)
+
+/* vector */
+template<class T> T vmax(vector<T> &array){ T ret = array[0]; for(T a: array) chmax(ret, a); return ret; }
+template<class T> T vmin(vector<T> &array){ T ret = array[0]; for(T a: array) chmin(ret, a); return ret; }
+template<class T> T sum(vector<T> &array){ T ret = 0; for(T a:array) ret += a; return ret; }
+template<class T> void list_set(vector<T> &array){ sort(all(array)); array.erase(unique(all(array)),array.end()); }
+template<class T> int bisect_left(vector<T> &array, T key){ return lower_bound(all(array),key) - array.begin(); }
+template<class T> int bisect_right(vector<T> &array, T key){ return upper_bound(all(array),key) - array.begin(); }
+
+/* string */
+ll string_to_ll(string n){ ll ret = 0, k = 1; while(n.length() > 0){ ret += k * (n.back() - '0'); n.pop_back(); k *= 10; } return ret; }
+string ll_to_string(ll n){ string ret = ""; while(n > 0){ ret.pb((n % 10) + '0'); n /= 10; } reverse(all(ret)); return ret; }
+
+struct popopo{ popopo(){ cin.tie(0); ios::sync_with_stdio(0); cout << fixed << setprecision(15); }; } popopoppo;
+
+// n = 1,...,N に対して、n % A < B を満たすものの数 
+ll Count_of_n_mod_A_less_than_B(ll N, ll A, ll B){ return N / A * min(A, B) + min(N % A, B - 1); }
+
+bool include(ll l, ll r, ll x) { return l <= x && x < r; }
+
+/* IN/OUT */
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &);
+template <class T> void scan(vector<T> &a) { for(auto &i : a) scan(i); }
+
+void IN(){}
+template<class Head, class... Tail> void IN(Head& head, Tail &...tail){ scan(head); IN(tail...); }
+
+#define INT(...) int __VA_ARGS__; IN(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__; IN(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__; IN(__VA_ARGS__)
+#define VEC(type, name, size) vector<type> name(size); IN(name)
+#define VECS(type, name, size) vector<type> name(size + 1); for(int i = 1; i <= size; i++) scan(name[i])
+
+void OUT(){ cout << "\n"; }
+template<class T> void output(T a){ cout << a; }
+template<class T> void output(vector<T> v){ for(int i = 0; i < v.size(); i++) cout << v[i] << (i == v.size() - 1 ? "" : " "); }
+template<class Head, class... Tail> void OUT(const Head &head, const Tail &...tail) { output(head); if(sizeof...(tail)) cout << " "; OUT(tail...); }
+
+void FLASH(){ cout << endl; }
+template<class Head, class... Tail> void FLASH(const Head  &head, const Tail &...tail) { output(head); if(sizeof...(tail)) cout << " "; FLASH(tail...); }
+
+namespace kyo {
+
+long long bisect(long long ok, long long ng, function<bool(long long)> is_ok) { while (abs(ok - ng) > 1) { long long mid = ok + (ng - ok) / 2; (is_ok(mid) ? ok : ng) = mid; } return ok; }
+
+}
+
+#define INCLUDED_MAIN
+#include __FILE__ // Codeforces で壊れるらしい
+
+#endif
